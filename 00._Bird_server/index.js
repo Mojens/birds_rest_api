41994251d494c8232b7a5889d6e9dc24f2e4b6e6
mojens <moha135h@stud.kea.dk>
2023-02-12 19:34:35 +0100

    made some todos so i know where to begin

---
+++ 00._Bird_server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+//todo Get all birds
+
+//todo Get one bird
+
+//todo Get all birds of a specific type
+
+//todo Get all birds of a specific type and color
+
+
+app.listen(8080);
