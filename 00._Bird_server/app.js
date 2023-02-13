43834129afca04d9d11cbe497c412212f84765ca
mojens <moha135h@stud.kea.dk>
2023-02-13 12:33:04 +0100

    edit / page

--- 00._Bird_server/app.js
+++ 00._Bird_server/app.js
@@ -12,6 +12,17 @@ const Birds = [
     { id: 6, type: "Swan", color: "White" },
 ];
 
+app.get('/', (req, res) => {
+    res.send(`<h1>Welcome to Mo's Bird Server!</h1>`
+    +`<h2>Available routes:</h2>`+
+    `<h3>
+    <br>/birds`+
+    `<br>/birds/:id`+
+    `<br>/birds?type=type`+
+    `<br>/birds?color=color`+
+    `<br>/birds?type=type&color=color
+    </h3>`);
+});
 
 app.get('/birds', (req, res) => {
     if (!req.query.type && !req.query.color) {
