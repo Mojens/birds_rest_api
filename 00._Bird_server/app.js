79ec76ce856dd1c72539a4d14ff0a99cfaf4727e
mojens <moha135h@stud.kea.dk>
2023-02-13 12:26:09 +0100

    one end point with different queries

--- 00._Bird_server/app.js
+++ 00._Bird_server/app.js
@@ -3,7 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-//todo Bird "database"
 const Birds = [
     { id: 1, type: 'Pigeon', color: 'White' },
     { id: 2, type: 'Eagle', color: 'Brown' },
@@ -40,7 +39,6 @@ app.get('/birds', (req, res) => {
     }
 });
 
-//todo Get one bird
 app.get('/birds/:id', (req, res) => {
     const bird = Birds.find(b => b.id === parseInt(req.params.id));
     if (!bird) {
