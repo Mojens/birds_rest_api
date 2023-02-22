418e526a887c85455154f2a0d71d411ed3590ad6
mojens <moha135h@stud.kea.dk>
2023-02-22 20:49:44 +0100

    bedre output

--- 01._Bird_server/app.js
+++ 01._Bird_server/app.js
@@ -5,21 +5,22 @@ const app = express();
 app.use(express.json());
 
 app.get('/birds', (req, res) => {
-    // at skrive ? er det samme som at sige req.query.type ? req.query.type.toLowerCase() : undefined;, det er så den ikke kaster en fejl ved at bruge toLowerCase på undefined
+    // at skrive ? er det samme som at sige req.query.type ? req.query.type.toLowerCase() : undefined;, 
+    // det er så den ikke kaster en fejl ved at bruge toLowerCase på undefined
     const type = req.query.type?.toLowerCase();
     const color = req.query.color?.toLowerCase();
     const filteredBirds = Birds.filter(bird => (
-      (!type || bird.type.toLowerCase() === type) &&
-      (!color || bird.color.toLowerCase() === color)
+        (!type || bird.type.toLowerCase() === type) &&
+        (!color || bird.color.toLowerCase() === color)
     ));
     res.send(filteredBirds);
-  });
-  
+});
+
 
 app.get('/birds/:id', (req, res) => {
     const bird = Birds.find(b => b.id === parseInt(req.params.id));
     if (!bird) {
-        res.status(404).send('Bird not found')
+        res.status(404).send(`Could not find Bird with ID: ${req.params.id}.`);
     };
     res.send(bird);
 });
@@ -36,7 +37,7 @@ app.post('/birds', (req, res) => {
 app.put('/birds/:id', (req, res) => {
     const bird = Birds.find(b => b.id === parseInt(req.params.id));
     if (!bird) {
-        res.status(404).send('Bird not found')
+        res.status(404).send(`Could not find Bird with ID: ${req.params.id}.`);
     };
     bird.type = req.body.type;
     bird.color = req.body.color;
@@ -45,11 +46,13 @@ app.put('/birds/:id', (req, res) => {
 app.patch('/birds/:id', (req, res) => {
     const bird = Birds.find(b => b.id === parseInt(req.params.id));
     if (!bird) {
-        res.status(404).send('Bird not found');
+        res.status(404).send(`Could not find Bird with ID: ${req.params.id}.`);
     } else {
+        // hvis der er en type, så skal den opdateres
         if (req.body.type) {
             bird.type = req.body.type;
         }
+        // hvis der er en color, så skal den opdateres
         if (req.body.color) {
             bird.color = req.body.color;
         }
@@ -59,13 +62,14 @@ app.patch('/birds/:id', (req, res) => {
 app.delete('/birds/:id', (req, res) => {
     const bird = Birds.find(b => b.id === parseInt(req.params.id));
     if (!bird) {
-        res.status(404).send('Bird not found');
-    };
+      res.status(404).send(`Could not find Bird with ID: ${req.params.id}.`);
+      return;
+    }
     const index = Birds.indexOf(bird);
-    // første argument er index, andet er antal
     Birds.splice(index, 1);
-    res.send(bird);
-});
+    res.send(`Deleted bird with ID ${req.params.id}.`);
+  });
+  
 
 app.listen(8080, () => {
     console.log('Server started on port 8080');
